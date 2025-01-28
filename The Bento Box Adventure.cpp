0186b63b2b32972e6a42777734719a8da853ceb2
Shadow-X26 <131441423+Shadow-X26@users.noreply.github.com>
2025-01-28 14:05:18 +0600

    Add files via upload

---
+++ The Bento Box Adventure.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+
+    int sum = a+b+c+d;
+
+    cout << 15-sum << endl;
+
+    return 0;
+}
