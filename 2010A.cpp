fe3c16e739108d87deee689222c40b6a64900019
Shadow-X26 <131441423+Shadow-X26@users.noreply.github.com>
2025-02-02 21:34:11 +0600

    Add files via upload

---
+++ 2010A.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector <int> a(n);
+        for (int i=0; i<n; i++){
+            cin >> a[i];
+        }
+        int sum = 0;
+        for (int i=0; i<n; i++){
+            if(i%2 == 0) {
+                sum += a[i];
+            }else{
+                sum -= a[i];
+            }
+        }
+        cout << sum << endl;
+    }
+    return 0;
+}
